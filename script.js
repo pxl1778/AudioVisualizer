e40e598ce5291e0d2310451a099d350ff272e7b0
pxl1778 <blackmateria55@aim.com>
2016-02-23 14:31:33 -0500

    Cleaning
    Cleaned up the code and removed unnecessary comments.

--- script.js
+++ script.js
@@ -12,15 +12,14 @@
 		var invert = false, tintRed = false, noise = false, lines = false;
 		
 		function init(){
-			// set up canvas stuff
+			//initializing variables
 			canvas = document.querySelector('canvas');
 			ctx = canvas.getContext("2d");
 			maxRadius = 200;
-			
-			// get reference to <audio> element on page
 			audioElement = document.querySelector('audio');
+			analyserNode = createWebAudioContextWithAnalyserNode(audioElement);
 			
-			//hook up circle radius
+			//hook up circle radius slider
 			document.querySelector('#circleRadiusSlider').onchange = function(e){
 				maxRadius = e.target.value;
 			};
@@ -39,77 +38,13 @@
 				lines = e.target.checked;
 			}
 			
-			// call our helper function and get an analyser node
-			analyserNode = createWebAudioContextWithAnalyserNode(audioElement);
-			
-			// get sound track <select> and Full Screen button working
 			setupUI();
-			
-			// load and play default sound into audio element
 			playStream(audioElement,SOUND_1);
-			
-			// start animation loop
 			update();
 		}
 		
-		
-		function createWebAudioContextWithAnalyserNode(audioElement) {
-			var audioCtx, analyserNode, sourceNode;
-			// create new AudioContext
-			// The || is because WebAudio has not been standardized across browsers yet
-			// http://webaudio.github.io/web-audio-api/#the-audiocontext-interface
-			audioCtx = new (window.AudioContext || window.webkitAudioContext);
-			
-			// create an analyser node
-			analyserNode = audioCtx.createAnalyser();
-			
-			/*
-			We will request NUM_SAMPLES number of samples or "bins" spaced equally 
-			across the sound spectrum.
-			
-			If NUM_SAMPLES (fftSize) is 256, then the first bin is 0 Hz, the second is 172 Hz, 
-			the third is 344Hz. Each bin contains a number between 0-255 representing 
-			the amplitude of that frequency.
-			*/ 
-			
-			// fft stands for Fast Fourier Transform
-			analyserNode.fftSize = NUM_SAMPLES;
-			
-			// this is where we hook up the <audio> element to the analyserNode
-			sourceNode = audioCtx.createMediaElementSource(audioElement); 
-			sourceNode.connect(analyserNode);
-			
-			// here we connect to the destination i.e. speakers
-			analyserNode.connect(audioCtx.destination);
-			return analyserNode;
-		}
-		
-		function setupUI(){
-			document.querySelector("#trackSelect").onchange = function(e){
-				playStream(audioElement,e.target.value);
-			};
-			
-			document.querySelector("#fsButton").onclick = function(){
-				requestFullscreen(canvas);
-			};
-		}
-		
-		function playStream(audioElement,path){
-			audioElement.src = path;
-			audioElement.play();
-			audioElement.volume = 0.2;
-			document.querySelector('#status').innerHTML = "Now playing: " + path;
-		}
-		
 		function update() { 
-			// this schedules a call to the update() method in 1/60 seconds
 			requestAnimationFrame(update);
-			
-			/*
-				Nyquist Theorem
-				http://whatis.techtarget.com/definition/Nyquist-Theorem
-				The array of data we get back is 1/2 the size of the sample rate 
-			*/
 			
 			// create a new array of 8-bit integers (0-255)
 			var data = new Uint8Array(NUM_SAMPLES/2); 
@@ -121,22 +56,16 @@
 			// OR
 			//analyserNode.getByteTimeDomainData(data); // waveform data
 			
-			// DRAW!
 			ctx.clearRect(0,0,800,600);  
 			var barWidth = 4;
 			var barSpacing = 1;
 			var barHeight = 100;
 			var topSpacing = 50;
 			
-			// loop through the data and draw!
 			for(var i=0; i<data.length; i++) { 
 				ctx.fillStyle = 'rgba(0,255,0,0.6)'; 
 				ctx.strokeStyle = 'rgba(0, 255, 0, 0.2)';
-				// the higher the amplitude of the sample (bin) the taller the bar
-				// remember we have to draw our bars left-to-right and top-down
-				/*ctx.fillRect(i * (barWidth + barSpacing),topSpacing + 256-data[i],barWidth,barHeight); 
-				ctx.fillRect(640 - i * (barWidth + barSpacing), topSpacing + 256 - data[i] - 20, barWidth, barHeight);
-				*/
+				
 				ctx.lineCap = "round";
 				ctx.lineWidth = 20;
 				ctx.beginPath();
@@ -179,6 +108,51 @@
 			manipulatePixels();
 		} 
 		
+		
+		function createWebAudioContextWithAnalyserNode(audioElement) {
+			var audioCtx, analyserNode, sourceNode;
+			audioCtx = new (window.AudioContext || window.webkitAudioContext);
+			analyserNode = audioCtx.createAnalyser();
+			
+			/*
+			We will request NUM_SAMPLES number of samples or "bins" spaced equally 
+			across the sound spectrum.
+			If NUM_SAMPLES (fftSize) is 256, then the first bin is 0 Hz, the second is 172 Hz, 
+			the third is 344Hz. Each bin contains a number between 0-255 representing 
+			the amplitude of that frequency.
+			*/ 
+			
+			// fft stands for Fast Fourier Transform
+			analyserNode.fftSize = NUM_SAMPLES;
+			
+			// this is where we hook up the <audio> element to the analyserNode
+			sourceNode = audioCtx.createMediaElementSource(audioElement); 
+			sourceNode.connect(analyserNode);
+			
+			// here we connect to the destination i.e. speakers
+			analyserNode.connect(audioCtx.destination);
+			return analyserNode;
+		}
+		
+		function setupUI(){
+			document.querySelector("#trackSelect").onchange = function(e){
+				playStream(audioElement,e.target.value);
+			};
+			
+			document.querySelector("#fsButton").onclick = function(){
+				requestFullscreen(canvas);
+			};
+		}
+		
+		function playStream(audioElement,path){
+			audioElement.src = path;
+			audioElement.play();
+			audioElement.volume = 0.2;
+			document.querySelector('#status').innerHTML = "Now playing: " + path;
+		}
+		
+		
+		
 		function manipulatePixels(){
 			var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 			var data = imageData.data;
@@ -197,8 +171,6 @@
 				}
 				if(noise && Math.random() < .10){
 					data[i] = data[i+1] = data[i+2] = 128;
-					//data[i] = data[i+1] = data[i+2] = 255;
-					//data[i] = data[i+1] = data[i+2] = 0;
 				}
 				if(lines){
 					var row = Math.floor(i/4/width);
@@ -211,13 +183,12 @@
 			}
 			ctx.putImageData(imageData, 0, 0);
 		}
-		// HELPER
+		
 		function makeColor(red, green, blue, alpha){
    			var color='rgba('+red+','+green+','+blue+', '+alpha+')';
    			return color;
 		}
 		
-		 // FULL SCREEN MODE
 		function requestFullscreen(element) {
 			if (element.requestFullscreen) {
 			  element.requestFullscreen();
@@ -228,7 +199,6 @@
 			} else if (element.webkitRequestFullscreen) {
 			  element.webkitRequestFullscreen();
 			}
-			// .. and do nothing if the method is not supported
 		};
 		
 		
